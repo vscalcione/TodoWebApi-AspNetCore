f6988e69075346fe4b4ffb417cdf899649ca5e5c
vscalcione <vincenzo99.scalcione@gmail.com>
2021-07-16 09:32:14 +0200

    Initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.Models {
+    public class TodoItem {
+        public long id { get; set; }
+        public string Name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
